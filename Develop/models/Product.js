31f51cdc323534cc6f87ddb3fcf060cfb6fea9a7
hrdowell <hannahrdowell@gmail.com>
2021-10-26 04:26:03 -0500

    edited post/put in models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -23,14 +23,21 @@ Product.init(
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       references: {
         model: "category",
         key: "id",
